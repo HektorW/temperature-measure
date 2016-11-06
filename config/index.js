1bb0217e3c0f93f18a7937f23e2c98bc8da9a628
Hektor Wallin <hektorw@gmail.com>
2016-11-06 15:05:21 +0100

    Better read large files

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const { env } = process
 module.exports = {
-  measurementInterval: env.MEASUREMENT_INTERVAL || 1000 * 60,
+  measurementInterval: env.MEASUREMENT_INTERVAL || 1000 * 60 * 10,
   mockSensor: env.MOCK_SENSOR === 'true' ? true : false,
   port: env.PORT || 1080,
   sensorLocation: env.SENSOR_LOCATION || 'unknown',
--- measurement/storage.js
+++ measurement/storage.js
@@ -1,4 +1,5 @@
-const { appendFile, readFile } = require('mz/fs')
+const { appendFile, readFile, stat } = require('mz/fs')
+const { createReadStream } = require('fs')
 const { join } = require('path')
 const co = require('co')
 const log = require('../log')('measurement/storage')
@@ -16,19 +17,49 @@ function write(output) {
   )
 }
 
-function read(count = 1, start = 0) {
-  return co(function* () {
-    const content = yield readFile(outputFile, 'utf8')
-    const allLines = content.split(/\n/g)
-    const selectedEntries = allLines
+// function read(count = 1, start = 0) {
+//   return co(function* () {
+//     const content = yield readFile(outputFile, 'utf8')
+//     const allLines = content.split(/\n/g)
+//     const selectedEntries = allLines
+//       .filter(line => /\{.+\}/.test(line))
+//       .reverse()
+//       .slice(start, start + count)
+//     return selectedEntries.map(entryStr => JSON.parse(entryStr))
+//   })
+// }
+
+
+function* readLarge(count = 1) {
+  const stats = yield stat(outputFile)
+  const fileSize = stats.size
+
+  const approxItemSize = 200
+  const readSize = approxItemSize * count
+  const startRead = fileSize - readSize
+  const fileContent = yield readPartialFile(outputFile, startRead)
+
+  const entries = fileContent.split(/\n/g)
+  const selectedEntries = entries
       .filter(line => /\{.+\}/.test(line))
       .reverse()
-      .slice(start, start + count)
-    return selectedEntries.map(entryStr => JSON.parse(entryStr))
+      .slice(0, count)
+
+  return selectedEntries.map(entryStr => JSON.parse(entryStr))
+}
+
+function readPartialFile(path, start) {
+  return new Promise((resolve, reject) => {
+    const stream = createReadStream(path, { start })
+    let content = ''
+    stream.on('data', chunk => content += chunk)
+    stream.on('end', () => resolve(content))
+    stream.on('error', error => reject(error))
   })
 }
 
 module.exports = {
   write,
-  read,
+  // read,
+  read: co.wrap(readLarge),
 }
